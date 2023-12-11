2be7a61ad805e9fab2520e0df9aecb7c8e3233f9
Anandita Singh <97512151+ananditasinghh@users.noreply.github.com>
2023-12-12 01:20:48 +0530

    Create project.js

---
+++ project.js
@@ -0,0 +1,11 @@
+
+    <script>
+        document.getElementById('navbar-toggle').addEventListener('click', function() {
+            var navbar = document.getElementById('navbar');
+            if (navbar.style.width === '200px') {
+                navbar.style.width = '0';
+            } else {
+                navbar.style.width = '200px';
+            }
+        });
+    </script>
